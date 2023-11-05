0fccb68de5ab97985f1d0a3675847b9c897370ab
hmiftachul11 <miftachul.huda@zettacamp.pro>
2023-11-05 12:34:13 +0700

    day 3

--- Day 2/no-5.js
+++ Day 2/no-5.js
@@ -6,15 +6,14 @@ let b = 2
 let c = 4;
 
 // Inisialisasi variabel maksimum dengan salah satu dari tiga bilangan
-let maksimum = a;
+let maksimum;
 
-// Periksa dan update nilai maksimum
-if (b > maksimum) {
+if (a >= b && a >= c) {
+    maksimum = a;
+} else if (b >= a && b >= c) {
     maksimum = b;
-}
-if (c > maksimum) {
+} else {
     maksimum = c;
 }
-
 // Tampilkan nilai maksimum
 console.log("Nilai maksimum dari tiga bilangan adalah: " + maksimum);
---
+++ Hands On Day 3/tabel-to-array.js
@@ -0,0 +1,40 @@
+const tableData = [
+    {
+      Nama: 'Zoro',
+      Nik: 322,
+      JenisKelamin: 'laki-laki',
+      GolonganDarah: 'O',
+      TempatAsal: 'Jakarta',
+      AlamatDomisili: 'Jayapura',
+      Hobi: ['Tidur', 'Gim', 'Minum Jus']
+    },
+    {
+      Nama: 'Lufi',
+      Nik: 246,
+      JenisKelamin: 'laki-laki',
+      GolonganDarah: 'B',
+      TempatAsal: 'Bekasi',
+      AlamatDomisili: 'Makassar',
+      Hobi: ['Makan', 'Memancing', 'Joging']
+    },
+    {
+      Nama: 'Sanji',
+      Nik: 311,
+      JenisKelamin: 'laki-laki',
+      GolonganDarah: 'A',
+      TempatAsal: 'Depok',
+      AlamatDomisili: 'Yogyakarta',
+      Hobi: ['Memasak', 'baca Buku', 'ngemil']
+    },
+    {
+      Nama: 'Nami',
+      Nik: 571,
+      JenisKelamin: 'Perempuan',
+      GolonganDarah: 'A',
+      TempatAsal: 'Bandung',
+      AlamatDomisili: 'Jakarta',
+      Hobi: ['Belanja', 'Perawatan', 'Tidur']
+    },
+];
+
+console.log(tableData);
