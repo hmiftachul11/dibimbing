5640bd537358c12fbd655341ba72d87fd0f18d06
hmiftachul11 <miftachul.huda@zettacamp.pro>
2023-11-19 20:38:44 +0700

    Hands On Day-7

--- Day-7/Hands-On-Day7/Task.js
+++ Day-7/Hands-On-Day7/Task.js
@@ -1,33 +1,31 @@
 function dashWord(sentence, interval) {
-    let startIndex = 0;
-    while (sentence[startIndex] === ' ' && startIndex < sentence.length) {
-      startIndex++;
-    }
-  
-    let endIndex = sentence.length - 1;
-    while (sentence[endIndex] === ' ' && endIndex >= 0) {
-      endIndex--;
-    }
-  
-    sentence = sentence.substring(startIndex, endIndex + 1);
-  
-    let result = '';
-  
-    for (let i = 0; i < sentence.length; i++) {
-      result += sentence[i];
-  
-      if ((i + 1) % interval === 0 && i !== sentence.length - 1) {
-        result += '-';
-      }
-    }
-  
-    return result;
+  let startIndex = 0;
+  while (sentence[startIndex] === " " && startIndex < sentence.length) {
+    startIndex++;
   }
-  
-  // TEST CASES
-  console.log(dashWord('   Semangat!   ', 3)); // Sem-ang-at!
-  console.log(dashWord(' Uzumaki Naruto ', 2)); // Uz-um-ak-i -Na-ru-to
-  console.log(dashWord(' 081234567890 ', 4)); // 0812-3456-7890
-  console.log(dashWord(' Indonesia Hebat ', 10)); // Indonesia -Hebat
-  console.log(dashWord(' Love Coding ', 1)); // L-o-v-e- -C-o-d-i-n-g
-  
+
+  let endIndex = sentence.length - 1;
+  while (sentence[endIndex] === " " && endIndex >= 0) {
+    endIndex--;
+  }
+
+  sentence = sentence.substring(startIndex, endIndex + 1);
+  let result = "";
+
+  for (let i = 0; i < sentence.length; i++) {
+    result += sentence[i];
+
+    if ((i + 1) % interval === 0 && i !== sentence.length - 1) {
+      result += "-";
+    }
+  }
+
+  return result;
+}
+
+// TEST CASES
+console.log(dashWord("   Semangat!   ", 3)); // Sem-ang-at!
+console.log(dashWord(" Uzumaki Naruto ", 2)); // Uz-um-ak-i -Na-ru-to
+console.log(dashWord(" 081234567890 ", 4)); // 0812-3456-7890
+console.log(dashWord(" Indonesia Hebat ", 10)); // Indonesia -Hebat
+console.log(dashWord(" Love Coding ", 1)); // L-o-v-e- -C-o-d-i-n-g
