1b9fdd6b36b38e3854eaa0fb3dd7c23bf8854dfc
hmiftachul11 <miftachul.huda@zettacamp.pro>
2023-11-19 20:45:14 +0700

    Hands On Day7

--- Day-7/Hands-On-Day7/Task.js
+++ Day-7/Hands-On-Day7/Task.js
@@ -24,8 +24,8 @@ function dashWord(sentence, interval) {
 }
 
 // TEST CASES
-console.log(dashWord("   Semangat!   ", 3)); // Sem-ang-at!
-console.log(dashWord(" Uzumaki Naruto ", 2)); // Uz-um-ak-i -Na-ru-to
-console.log(dashWord(" 081234567890 ", 4)); // 0812-3456-7890
-console.log(dashWord(" Indonesia Hebat ", 10)); // Indonesia -Hebat
-console.log(dashWord(" Love Coding ", 1)); // L-o-v-e- -C-o-d-i-n-g
+console.log(dashWord("Semangat!", 3)); // Sem-ang-at!
+console.log(dashWord("Uzumaki Naruto", 2)); // Uz-um-ak-i -Na-ru-to
+console.log(dashWord("081234567890", 4)); // 0812-3456-7890
+console.log(dashWord("Indonesia Hebat", 10)); // Indonesia -Hebat
+console.log(dashWord("Love Coding", 1)); // L-o-v-e- -C-o-d-i-n-g
