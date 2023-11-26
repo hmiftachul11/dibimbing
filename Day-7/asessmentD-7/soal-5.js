53658dac6bbbbf97080b2d6aaab158498ce13c69
hmiftachul11 <miftachul.huda@zettacamp.pro>
2023-11-26 23:28:44 +0700

    Changes soal-5.js

--- Day-7/asessmentD-7/soal-5.js
+++ Day-7/asessmentD-7/soal-5.js
@@ -19,22 +19,52 @@ const data = {
   total: 3,
 };
 
+function getMonthName(month) {
+  const monthNames = [
+    "Januari",
+    "Februari",
+    "Maret",
+    "April",
+    "Mei",
+    "Juni",
+    "Juli",
+    "Agustus",
+    "September",
+    "Oktober",
+    "November",
+    "Desember",
+  ];
+  return monthNames[parseInt(month, 10) - 1];
+}
+
 function PrintDataSiswa(data) {
   for (let i = 0; i < data.students.length; i++) {
-    const [no, nama, tempat, tanggalLahir, hobi] = data.students[i];
+    const student = data.students[i];
 
     console.log(`# Siswa ${i + 1}`);
-    console.log(` - no: ${no}`);
-    console.log(` - nama: ${nama}`);
+    console.log(` - no: ${student[0]}`);
+    console.log(` - nama: ${student[1]}`);
 
-    const formattedDate = tanggalLahir
-      .split("/")
-      .map((part, index) => (index === 1 ? part + " Mei " : part))
-      .join(" ");
+    const tanggalLahirParts = [];
+    let part = "";
+    for (let j = 0; j < student[3].length; j++) {
+      if (student[3][j] === "/") {
+        tanggalLahirParts.push(part);
+        part = "";
+      } else {
+        part += student[3][j];
+      }
+    }
+    tanggalLahirParts.push(part);
 
-    console.log(` - tempat, tanggal lahir: ${tempat}, ${formattedDate}`);
+    const formattedDate = `${tanggalLahirParts[0]} ${getMonthName(
+      tanggalLahirParts[1]
+    )} ${tanggalLahirParts[2]}`;
+    console.log(` - tempat, tanggal lahir: ${student[2]}, ${formattedDate}`);
 
-    const formattedHobbies = Array.isArray(hobi) ? hobi.join(", ") : hobi;
+    const formattedHobbies = Array.isArray(student[4])
+      ? student[4].join(", ")
+      : student[4];
     console.log(` - hobi: ${formattedHobbies}\n`);
   }
 }
