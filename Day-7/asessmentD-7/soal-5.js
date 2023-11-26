0d0f86bc580fe74541450c218d24a077ba6a8fc9
hmiftachul11 <miftachul.huda@zettacamp.pro>
2023-11-26 23:34:01 +0700

    Hilangkan Method JS

--- Day-7/asessmentD-7/soal-5.js
+++ Day-7/asessmentD-7/soal-5.js
@@ -34,38 +34,68 @@ function getMonthName(month) {
     "November",
     "Desember",
   ];
-  return monthNames[parseInt(month, 10) - 1];
+  let result = "";
+  let i = 0;
+  while (i < monthNames.length) {
+    if (i + 1 == month) {
+      result = monthNames[i];
+      break;
+    }
+    i++;
+  }
+  return result;
+}
+
+function extractDateParts(dateString) {
+  let part = "";
+  const parts = [];
+  let isParsingDay = true;
+  let i = 0;
+  while (i < dateString.length) {
+    if (dateString[i] === "/") {
+      parts[parts.length] = part;
+      part = "";
+      isParsingDay = !isParsingDay;
+    } else {
+      part += dateString[i];
+    }
+    i++;
+  }
+  parts[parts.length] = part;
+  return parts;
 }
 
 function PrintDataSiswa(data) {
-  for (let i = 0; i < data.students.length; i++) {
+  let i = 0;
+  while (i < data.students.length) {
     const student = data.students[i];
 
     console.log(`# Siswa ${i + 1}`);
     console.log(` - no: ${student[0]}`);
     console.log(` - nama: ${student[1]}`);
 
-    const tanggalLahirParts = [];
-    let part = "";
-    for (let j = 0; j < student[3].length; j++) {
-      if (student[3][j] === "/") {
-        tanggalLahirParts.push(part);
-        part = "";
-      } else {
-        part += student[3][j];
-      }
-    }
-    tanggalLahirParts.push(part);
-
+    const tanggalLahirParts = extractDateParts(student[3]);
     const formattedDate = `${tanggalLahirParts[0]} ${getMonthName(
-      tanggalLahirParts[1]
+      parseInt(tanggalLahirParts[1], 10)
     )} ${tanggalLahirParts[2]}`;
     console.log(` - tempat, tanggal lahir: ${student[2]}, ${formattedDate}`);
 
-    const formattedHobbies = Array.isArray(student[4])
-      ? student[4].join(", ")
-      : student[4];
+    let formattedHobbies = "";
+    let j = 0;
+    if (Array.isArray(student[4])) {
+      while (j < student[4].length) {
+        formattedHobbies += student[4][j];
+        if (j < student[4].length - 1) {
+          formattedHobbies += ", ";
+        }
+        j++;
+      }
+    } else {
+      formattedHobbies = student[4];
+    }
+
     console.log(` - hobi: ${formattedHobbies}\n`);
+    i++;
   }
 }
 
